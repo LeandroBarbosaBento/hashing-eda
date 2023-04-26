114e4640e566acc47e02e7808eb45bb615b7cea9
leandro <leandro@pop-os.localdomain>
2023-04-26 17:05:33 -0300

    criação de structs para hashing duplo e função de criação do hashing e exibição de dados

--- include/double_hashing.h
+++ include/double_hashing.h
@@ -2,7 +2,29 @@
 #define __DOUBLE_HASHING
 
 #include <stdio.h>
-//opens the log file for writing
-FILE * nomeDaFuncaoDouble(char * path);
+#include <stdlib.h>
+
+typedef struct double_item {
+  int key;
+  int is_empty;
+} double_item_t;
+
+typedef struct double_hashing {
+  int total_access;
+  int size;
+  double_item_t *v;
+  int number_of_keys;
+} double_hashing_t;
+
+double_hashing_t create_double_hashing(int n);
+
+double_hashing_t * insert_double_hashing(double_hashing_t * x, int key);
+
+double_hashing_t * get_avarage_double_hashing(double_hashing_t * x);
+
+void show_data_double_hashing(double_hashing_t * x);
+
+void menu_double_hashing();
+
 
 #endif
--- src/double_hashing/double_hashing.c
+++ src/double_hashing/double_hashing.c
@@ -1,7 +1,32 @@
 #include "double_hashing.h"
 
 // Open the log-file in append mode and return it
-FILE * nomeDaFuncaoDouble(char * file_path) {
-    FILE * fp = fopen(file_path, "a");
-    return fp;
+double_hashing_t create_double_hashing(int n)
+{
+    double_hashing_t x;
+
+    x.v = malloc( (n) * sizeof(double_item_t) );
+    x.total_access = 0;
+    x.size = n;
+    x.number_of_keys = 0;
+    
+    printf("Teste n   : %d\n", n);
+
+    for(int i = 0; i <= n; i++){
+        x.v[i].is_empty = 1;
+        x.v[i].key = -1;
+    }
+
+    return x;
+}
+
+void show_data_double_hashing(double_hashing_t * x)
+{
+    printf("total_access: %d\n", x->total_access);
+    printf("size: %d\n", x->size);
+    printf("number_of_keys: %d\n", x->number_of_keys);
+
+    for(int i = 0; i < x->size; i++){
+        printf("position %d: k: %d | is_empty: %d\n", i,x->v[i].key, x->v[i].is_empty);
+    }
 }
