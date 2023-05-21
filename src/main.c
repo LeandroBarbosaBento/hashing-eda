f74e86bb2501c4ef14960872785634334b6b24c5
leandro <leandro@pop-os.localdomain>
2023-05-21 11:51:42 -0300

    changes on main for reading txt file

--- src/main.c
+++ src/main.c
@@ -1,93 +1,62 @@
 #include "chaining.h"
 #include "double_hashing.h"
 #include "linear_probing.h"
+#include "hashing_perfeito.h"
 
 
+int main(int argc, char *argv[]) {
 
-int main() {
+    if (argc != 2) {
+        printf("Use: %s <file_name>\n", argv[0]);
+        return 1;
+    }
 
-    /***********CHAINING HASHING*********/
+    FILE *file;
+    char *data = NULL;
+    size_t size = 0;
+    ssize_t charRead;
+    int isFirst = 1;
+    int hashing_file_size = 0;
 
-    /* chaining_hashing_t chaining_hashing;
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("File oppening error.\n");
+        return 1;
+    }
 
-    int n = 11;
-
-    chaining_hashing = create_chaining_hashing(n);
-
-    show_data_chaining_hashing(&chaining_hashing);
-
-    insert_chaining_hashing(&chaining_hashing, 5);
-    insert_chaining_hashing(&chaining_hashing, 16);
-    insert_chaining_hashing(&chaining_hashing, 27);
-
-    printf("\n\nAPÓS INSERIR 5, 16 e 27: \n\n");
-
-    show_data_chaining_hashing(&chaining_hashing);
-
-    printf("\n"); */
-
-
-
-    
-
-    /* double_hashing_t double_hashing;
-
-    int n = 11;
-
-    double_hashing = create_double_hashing(n);
-
-    show_data_double_hashing(&double_hashing);
-
-    insert_double_hashing(&double_hashing, 5);
-    //insert_double_hashing(&double_hashing, 16);
-    insert_double_hashing(&double_hashing, 27);
-
-    printf("\n\nAPÓS INSERIR 5, 16 e 27: \n\n");
-
-    show_data_double_hashing(&double_hashing);
-
-    printf("\n"); */
-
-    /***********LINEAR PROBING HASHING*********/
-
-    /* int n = 11;
-    linear_probing_hashing_t linear_probing_hashing;
-
-    printf("Criando linear hashing...\n");
-
-    linear_probing_hashing = create_linear_probing_hashing(n);
-
-    show_data_linear_probing_hashing(&linear_probing_hashing);
-
-    insert_linear_probing_hashing(&linear_probing_hashing, 5);
-    insert_linear_probing_hashing(&linear_probing_hashing, 8);
-    insert_linear_probing_hashing(&linear_probing_hashing, 19);
-    insert_linear_probing_hashing(&linear_probing_hashing, 30);
-    insert_linear_probing_hashing(&linear_probing_hashing, 52);
-
-    printf("\nInseriu 5, 8, 19, 30, 52 \n");
-
-    show_data_linear_probing_hashing(&linear_probing_hashing); */
-
-
-    /***********DOUBLE HASHING*********/
-
-    int n = 11;
+    chaining_hashing_t chaining_hashing;
     double_hashing_t double_hashing;
+    linear_probing_hashing_t linear_probing_hashing;
+    hashing_perfeito_t hashing_perfeito;
 
-    printf("Criando double hashing...\n");
+    while ((charRead = getline(&data, &size, file)) != -1) {
+        int number = (int) strtol(data, NULL, 10);
+        if (isFirst)
+        {
+            hashing_file_size = number;
+            isFirst = 0;
+            break;
+        }
+    }
 
-    double_hashing = create_double_hashing(n);
+    chaining_hashing = create_chaining_hashing(hashing_file_size);
+    printf("Teste 01 \n");
+    double_hashing = create_double_hashing(hashing_file_size);
+    printf("Teste 02 \n");
+    linear_probing_hashing = create_linear_probing_hashing(hashing_file_size);
+    printf("Teste 03 \n");
+    hashing_perfeito =  create_hashing_perfeito(hashing_file_size, 100000);
 
+    while ((charRead = getline(&data, &size, file)) != -1) {
+        int number = (int) strtol(data, NULL, 10);
+        insert_chaining_hashing(&chaining_hashing, number);
+        insert_linear_probing_hashing(&linear_probing_hashing, number);
+        insert_double_hashing(&double_hashing, number);
+    }
+
+    show_data_chaining_hashing(&chaining_hashing);
+    show_data_linear_probing_hashing(&linear_probing_hashing);
     show_data_double_hashing(&double_hashing);
 
-    insert_double_hashing(&double_hashing, 5);
-    insert_double_hashing(&double_hashing, 8);
-    insert_double_hashing(&double_hashing, 19);
-    insert_double_hashing(&double_hashing, 30);
-    insert_double_hashing(&double_hashing, 52);
-
-    printf("\nInseriu 5, 8, 19, 30, 52 \n");
-
-    show_data_double_hashing(&double_hashing);
+    return 0;
 }
